edb682f8a26192dfdc7d659be8ea9f53d8d80de3
Tom Kay <oridan82@gmail.com>
2019-11-12 16:17:14 +0000

    specify tag/version

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-git clone --depth=1 --branch=trunk git@github.com:apache/cassandra tmp
+
+TAG=$1
+
+if [[ "$TAG" == "" ]]; then
+  echo "no branch or tag specified"
+  exit 1
+fi
+
+git clone --depth=1 --branch=$TAG git@github.com:apache/cassandra tmp
 cp tmp/interface/cassandra.thrift cassandra-thrift/
 rm -Rf tmp
 thrift --gen php:oop,validate cassandra-thrift/cassandra.thrift
