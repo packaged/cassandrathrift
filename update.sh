f58600fa290ae37dfcfcf5e2a7cfa0cf97d2ec2b
Tom Kay <oridan82@gmail.com>
2019-11-26 10:29:10 +0000

    remove old files

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ fi
 git clone --depth=1 --branch=$TAG git@github.com:apache/cassandra tmp
 cp tmp/interface/cassandra.thrift cassandra-thrift/
 rm -Rf tmp
+rm -Rf src/cassandra
 thrift --gen php:oop,validate cassandra-thrift/cassandra.thrift
 cp -R gen-php/cassandra src
 rm -Rf gen-php/
-php refactor.php
